7ea3038d397c6f0416b7bb2b140b1a774be68aca
Anusha Yadandla <es14btech11024@iith.ac.in>
2017-11-02 20:15:40 +0530

    File to extract dataset

---
+++ data.py
@@ -0,0 +1,27 @@
+#returns the data
+
+import h5py
+import numpy as np
+
+
+def getData():
+    # reads the data from a file
+	# mat file containing the feature vectors obtained from dense SIFT descriptors
+    f = h5py.File('Ftraining_denseSIFT_split_01.mat','r') 
+        
+    arrays = {}
+    for k, v in f.items():
+        arrays[k] = np.array(v)
+
+    # extracting the required data array
+    dense = arrays['Ftraining'] 
+	# printing the original data file size  
+    print(dense.shape)
+    # reshaping the data matrix
+    dense = dense[0:50,0:200]
+    print(dense.shape)
+    
+    #dense = dense[:,0:20000]
+    lC = 200
+	# lC is the num of dimensions after reducing the dimensionality
+    return dense,lC
